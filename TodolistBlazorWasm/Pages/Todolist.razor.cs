a585e6a3bc1004fccb22b801f59c9a0f12c0904f
MrVuThanhCong <mr.vtcsharp@gmail.com>
2021-06-10 10:51:23 +0700

    Learn component communication for feature "SearchTask"

--- TodolistBlazorWasm/Pages/Todolist.razor.cs
+++ TodolistBlazorWasm/Pages/Todolist.razor.cs
@@ -12,19 +12,20 @@ namespace TodolistBlazorWasm.Pages
     public partial class Todolist // code behind
     {
         [Inject] private ITaskAPIClient TaskAPIClient { get; set; }
-
+        
         private List<TaskDTO> Tasks;
 
-        
-
-        private TaskListSearch TaskLiskSearch = new TaskListSearch();
+        private TaskListSearch TaskListSearch = new TaskListSearch();
         protected override async Task OnInitializedAsync()
         {
-            Tasks = await TaskAPIClient.GetTaskList(TaskLiskSearch);
-
-            
+            Tasks = await TaskAPIClient.GetTaskList(TaskListSearch);
         }
         
+        public async Task SearchTask(TaskListSearch taskListSearch)
+        {
+            TaskListSearch = taskListSearch;
+            Tasks = await TaskAPIClient.GetTaskList(TaskListSearch);
+        }
     }
 
     
